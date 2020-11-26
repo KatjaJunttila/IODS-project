46a728d6ea885da2ad2a9d47fca8934a32a65045
Katja <katja.junttila@helsinki.fi>
2020-11-26 23:11:18 +0200

    Exercise 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,4 +1,5 @@
 # Katja Junttila, 19.11.2020, Exercise 4
+# Includes also work from 23.11.2020, Exercise 5, see below
 
 # Wrangling "Human development" data from http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv
 # and "Gender inequality" data from http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv
@@ -36,3 +37,51 @@ human <- inner_join(hd, gii, by = "country")
 
 # Save the new human data set
 write.csv(human, "data/human.csv", row.names = F)
+
+# Exercise 5 starts here
+
+# Load the human data
+human <- read.csv("data/human.csv")
+
+# The data set originates from the United Nations Development Programme.
+# It consists of indicators of human development.
+# Explanations of variables:
+# expedu = expected years of schooling
+# explife = life expectancy at birth
+# gni = gross national Income per capita
+# matmor = maternal mortality ratio
+# adobirth = adolescent birth rate
+# fparl = percetange of female representatives in parliament
+# eduratio = ratio of females to males with at least secondary education
+# labratio = ratio of females to males in the labour force
+
+# Explore the structure and dimensions of the data
+str(human)
+dim(human)
+
+# Access stringr
+library(stringr)
+
+# Transform the gni variable to numeric
+human$gni <- str_replace(human$gni, pattern=",", replace ="") %>% as.numeric
+
+# Exclude unneeded variables
+keep <- c("country", "eduratio", "labratio", "expedu", "explife", "gni",
+          "matmor", "adobirth", "fparl")
+human <- select(human, one_of(keep))
+
+# Remove rows with missing values
+human <- filter(human, complete.cases(human))
+
+# Remove observations related to regions instead of countries
+last <- nrow(human) - 7
+human <- human[1:last,]
+
+# Define row names by country names
+rownames(human) <- human$country
+
+# Remove the country column
+human <- select(human, -country)
+
+# Save the new human data set
+write.csv(human, "data/human.csv", row.names = T)
